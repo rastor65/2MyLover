349b846aa5a243ea2a8028285baee69a4e1b9111
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 23:38:42 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/2MyLover' : '',
-  images: { unoptimized: true },
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
