e1d55a6f429d8b68c7ad7c550536cf323535b082
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 23:26:07 -0500

    2MyLover

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  //basePath: process.env.NODE_ENV === 'production' ? '/cafe-producciones' : '',
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default nextConfig;
